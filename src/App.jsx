185e5807b0b7fa847fde40b629a201e1b7a60909
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-19 14:43:20 +0500

    Split Form

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,11 @@ function Header(){
 
 function Profile({setShowAddFriend}){
   return <>
-          <div>
+  <div className="flex">
+          <div className="w-8/12">
             <span>
               {initialFriends.map((friend)=>{
-                return <div className="inline-flex justify-between w-3/4 m-5 p-3 border border-blue-700 shadow-sm shadow-blue-900" key={friend.id}>
+                return <div className="inline-flex justify-between w-11/12 m-5 p-3 border border-blue-700 shadow-sm shadow-blue-900" key={friend.id}>
                   <span>
                     <img className="bg-blue-200 shadow-md shadow-blue-900  p-1 mr-2 w-10 h-10 rounded-full" src={friend.image} alt={friend.name} />
                   </span>
@@ -48,6 +49,9 @@ function Profile({setShowAddFriend}){
               })}
             </span>
             <Button onClick={()=>setShowAddFriend(true)}>Add Friend</Button>
+            
+          </div>
+          <div className=" m-5 p-3 "><SplitForm /></div>
           </div>
         </>
 }
@@ -59,7 +63,7 @@ function Button({children, onClick}){
 }
 
 function FriendForm({setShowAddFriend}) {
-  return <form className="m-5 border-2 border-blue w-3/4 shadow-sm pl-4 py-5 shadow-black">
+  return <form className="m-5 border-2 border-blue w-11/12 shadow-sm pl-4 py-5 shadow-black">
     <div>
             <label htmlFor="name" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Name</label>
             <input type="text" id="name" className="mx-3 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
@@ -69,6 +73,28 @@ function FriendForm({setShowAddFriend}) {
             <input type="text" id="image" className="mx-3 mb-5 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="image.png" required />
         </div>
     <Button onClick={()=>setShowAddFriend(false)}>Close</Button>
+  </form>
+}
+
+function SplitForm({setShowAddFriend}) {
+  return <form className="m-5 shadow-sm p-4 shadow-black">
+    <div>
+            <label htmlFor="expense" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total Expense</label>
+            <input type="text" id="expense" className="mx-3 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1000" required />
+        </div>
+        <div>
+            <label htmlFor="splitter" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Split with: </label>
+            <input type="text" id="image" className="mx-3 mb-5 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Choose a friend" required />
+        </div>
+        <div>
+            <label htmlFor="amount" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Split Amount</label>
+            <input type="text" id="image" className="mx-3 mb-5 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="image.png" required />
+        </div>
+        <div>
+            <label htmlFor="resturant" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Resturant</label>
+            <input type="text" id="image" className="mx-3 mb-5 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Resturant Name" required />
+        </div>
+    <Button onClick={()=>setShowAddFriend(false)}>Split</Button>
   </form>
 }
 
