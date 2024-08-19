59aa87db5dd803bbc1f8f03af0a44792f29073b9
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-19 12:32:45 +0500

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useState } from "react"
 import { initialFriends } from "../public/data"
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
   return (<>
-  <Profile />
-  <FriendForm />
+  <div className="">
+  <Header />
+  <Profile setShowAddFriend={setShowAddFriend} />
+  { showAddFriend && <FriendForm setShowAddFriend={setShowAddFriend} />}
+  </div>
+  
   </>
     
   )
 }
 
-function Profile(){
+function Header(){
+  return (<div className=" text-white font-bold font-serif p-3 text-center bg-gradient-to-t from-blue-700 via-blue-300 to-blue-700">
+    Eat & Split
+  </div>)
+}
+
+function Profile({setShowAddFriend}){
   return <>
           <div>
             <span>
@@ -33,23 +47,28 @@ function Profile(){
                 </div> 
               })}
             </span>
+            <Button onClick={()=>setShowAddFriend(true)}>Add Friend</Button>
           </div>
         </>
 }
 
-function Button({children}){
-  return <div className="end-0 ">
-  <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-4 py-2 text-center ml-5 mt-1">{children}</button>
+function Button({children, onClick}){
+  return <div className="">
+  <button onClick={onClick} className="m-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-4 py-2 text-center ml-5 mt-1">{children}</button>
 </div>
 }
 
-function FriendForm() {
-  return <form>
-    <label htmlFor="addFriend">Friend Name</label>
-    <input type="text"></input>
-    <label htmlFor="addFriend">Friend Image</label>
-    <input className="border-2 border-black" type="text"></input>
-    <Button>Add Friend</Button>
+function FriendForm({setShowAddFriend}) {
+  return <form className="m-5 border-2 border-blue w-3/4 shadow-sm pl-4 py-5 shadow-black">
+    <div>
+            <label htmlFor="name" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Name</label>
+            <input type="text" id="name" className="mx-3 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+        </div>
+        <div>
+            <label htmlFor="image" className="m-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Image URL</label>
+            <input type="text" id="image" className="mx-3 mb-5 w-11/12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="image.png" required />
+        </div>
+    <Button onClick={()=>setShowAddFriend(false)}>Close</Button>
   </form>
 }
 
